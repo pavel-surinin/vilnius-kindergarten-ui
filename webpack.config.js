622940a8a546bf6cc4d41c707b6b00f8450f5ba3
Pavel Surinin <psurinin@eisgroup.com>
2017-07-13 09:42:19 +0300

    added plugin to create index.html and insert bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,50 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
 
 module.exports = {
-  entry: './src/App.js',
-  output: { path: path.resolve(__dirname, 'build'), filename: 'bundle.js' },
-  module: {
-    loaders: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {
- test: /\.css$/,
- // https://github.com/webpack-contrib/css-loader
- use: [
-    'style-loader',
-    {
-      loader: 'css-loader',
-      options: {
-       modules: true,
-       localIdentName: '[path][name]__[local]--[hash:base64:5]'
-     }
+    entry: './src/App.js',
+    output: {path: path.resolve(__dirname, 'build'), filename: 'bundle.js'},
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                // https://github.com/webpack-contrib/css-loader
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[path][name]__[local]--[hash:base64:5]'
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader',
+                    'css-loader',
+                    'less-loader'],
+            },
+        ]
     },
-  ]
-},
-{
-  test: /\.less$/,
-  use: ['style-loader',
-        'css-loader', 
-        'less-loader'],
-},
-    ]
-  },
-  stats: {
-      colors: true
-  },
-  devtool: 'source-map'
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    plugins : [HTMLWebpackPluginConfig]
 };
